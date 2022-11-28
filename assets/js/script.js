38509ce1c9f74be7a54d80c62813edbffa4e2eff
DOX7 <115791497+DarrenOxley7@users.noreply.github.com>
2022-11-28 15:17:42 +0000

    formats script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,35 +96,31 @@ var upperCasedCharacters = [
 function getPasswordOptions() {
   // User selects the password length
   var passwordLength = prompt("Please choose the length of your password. Must be between 10 and 64 characters");
-  
+
   while (passwordLength < 10 || passwordLength > 64 || isNaN(parseInt(passwordLength))) {
     var passwordLength = prompt("You need to type a number between 10 and 64");
   }
 
   // User chooses character types to use in PW
   var choices = {
-    "uppercaseChars": confirm ("Would you like your password to contain uppercase letters?"),
-    "lowercaseChars": confirm ("Would you like your password to contain lowercase letters?"),
-    "numericChars": confirm ("Would you like your password to contain numbers?"),
-    "specialChars": confirm ("Would you like your password to contain special characters?"),
-    }
+    "uppercaseChars": confirm("Would you like your password to contain uppercase letters?"),
+    "lowercaseChars": confirm("Would you like your password to contain lowercase letters?"),
+    "numericChars": confirm("Would you like your password to contain numbers?"),
+    "specialChars": confirm("Would you like your password to contain special characters?"),
+  }
 
 
   // User selections and starter data arrays concatinated to create final choices array
   userChoices = []
 
   for (var index in choices) {
-    if (index == "uppercaseChars" && choices[index])
-      {userChoices = userChoices.concat(upperCasedCharacters)}
+    if (index == "uppercaseChars" && choices[index]) { userChoices = userChoices.concat(upperCasedCharacters) }
 
-    else if (index == "lowercaseChars" && choices[index])
-      {userChoices = userChoices.concat(lowerCasedCharacters)}
+    else if (index == "lowercaseChars" && choices[index]) { userChoices = userChoices.concat(lowerCasedCharacters) }
 
-    else if (index == "numericChars" && choices[index])
-      {userChoices = userChoices.concat(numericCharacters)}
+    else if (index == "numericChars" && choices[index]) { userChoices = userChoices.concat(numericCharacters) }
 
-    else if (index == "specialChars" && choices[index])
-      {userChoices = userChoices.concat(specialCharacters)}
+    else if (index == "specialChars" && choices[index]) { userChoices = userChoices.concat(specialCharacters) }
   }
 
   // get a list with choices and length
@@ -142,8 +138,8 @@ function generatePassword() {
   //For the selected length, each for loop picks a character at random from the selected data sets and adds them to the generatedPassord var to give the final password.
   //negates the need for the getRandom(arr) which has been removed from previous commits
 
-  for (var i=0; i<userChoicesPasswordLength[1]; i++) {
-    passwordElement = userChoicesPasswordLength[0] [Math.floor(Math.random()*userChoicesPasswordLength[0].length)];
+  for (var i = 0; i < userChoicesPasswordLength[1]; i++) {
+    passwordElement = userChoicesPasswordLength[0][Math.floor(Math.random() * userChoicesPasswordLength[0].length)];
     generatedPassword += passwordElement
   };
 
